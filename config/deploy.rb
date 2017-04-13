aa4f4c6b712127f8cadd638c67b557b3f0d2f5ac
Juan Fernando <juanfer2696@hotmail.com>
2017-04-13 17:22:13 -0500

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # Change these
-server 'your_server_ip', port: your_port_num, roles: [:web, :app, :db], primary: true
+server '104.236.58.183', port: 80, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@example.com:username/appname.git'
-set :application,     'appname'
+set :repo_url,        'https://github.com/jossava/RoRBlog.git'
+set :application,     'blog'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
